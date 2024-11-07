e0ad3eef0e5cfd231a6573bf7162c96639e084a8
kbizikav <132550763+kbizikav@users.noreply.github.com>
2024-11-08 01:25:00 +0700

    get block merkle proof

--- src/external_api/block_validity_prover/server/mod.rs
+++ src/external_api/block_validity_prover/server/mod.rs
@@ -1,7 +1,8 @@
 pub mod account_id;
-pub mod tx_tree_root_status;
+pub mod block_merkle_proof;
 pub mod conversion;
 pub mod deposit_index;
 pub mod info;
+pub mod tx_tree_root_status;
 pub mod update_witness;
 pub mod validity_pis;
