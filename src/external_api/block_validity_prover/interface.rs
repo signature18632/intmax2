a9d1429c2082f23cced471a50eeaa7d4eaf62973
kbizikav <132550763+kbizikav@users.noreply.github.com>
2024-11-07 20:51:47 +0700

    block validity prover

---
+++ src/external_api/block_validity_prover/interface.rs
@@ -0,0 +1,73 @@
+use async_trait::async_trait;
+use intmax2_zkp::{
+    circuits::validity::validity_pis::ValidityPublicInputs,
+    common::{
+        trees::{
+            account_tree::AccountMembershipProof, block_hash_tree::BlockHashMerkleProof,
+            deposit_tree::DepositMerkleProof, sender_tree::SenderLeaf,
+        },
+        witness::update_witness::UpdateWitness,
+    },
+    ethereum_types::{bytes32::Bytes32, u256::U256},
+};
+use plonky2::{field::goldilocks_field::GoldilocksField, plonk::config::PoseidonGoldilocksConfig};
+
+use crate::external_api::common::error::ServerError;
+
+type F = GoldilocksField;
+type C = PoseidonGoldilocksConfig;
+const D: usize = 2;
+
+#[async_trait]
+pub trait BlockValidityInterface {
+    // Returns the latest block number which the block validity prover synced to.
+    async fn block_number(&self) -> Result<u32, ServerError>;
+
+    async fn get_account_id(&self, pubkey: U256) -> Result<Option<usize>, ServerError>;
+
+    async fn get_update_witness(
+        &self,
+        pubkey: U256,
+        root_block_number: u32,
+        leaf_block_number: u32,
+        is_prev_account_tree: bool,
+    ) -> Result<UpdateWitness<F, C, D>, ServerError>;
+
+    async fn get_deposit_index_and_block_number(
+        &self,
+        deposit_hash: Bytes32,
+    ) -> Result<Option<(usize, u32)>, ServerError>;
+
+    async fn get_block_number_by_tx_tree_root(
+        &self,
+        tx_tree_root: Bytes32,
+    ) -> Result<Option<u32>, ServerError>;
+
+    async fn get_validity_pis(
+        &self,
+        block_number: u32,
+    ) -> Result<Option<ValidityPublicInputs>, ServerError>;
+
+    async fn get_sender_leaves(
+        &self,
+        block_number: u32,
+    ) -> Result<Option<Vec<SenderLeaf>>, ServerError>;
+
+    async fn get_block_merkle_proof(
+        &self,
+        root_block_number: u32,
+        leaf_block_number: u32,
+    ) -> Result<Vec<BlockHashMerkleProof>, ServerError>;
+
+    async fn get_account_membership_proof(
+        &self,
+        block_number: u32,
+        pubkey: U256,
+    ) -> Result<AccountMembershipProof, ServerError>;
+
+    async fn get_deposit_merkle_proof(
+        &self,
+        block_number: u32,
+        deposit_index: usize,
+    ) -> Result<DepositMerkleProof, ServerError>;
+}
