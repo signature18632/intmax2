dd90b5f58ddd36533dfb989b61ee4da88323a2c8
kbizikav <132550763+kbizikav@users.noreply.github.com>
2024-11-14 03:51:58 +0700

    remove comment

--- test-server/src/api/contract/types.rs
+++ test-server/src/api/contract/types.rs
@@ -1,15 +1,3 @@
-// impl ContractInterface for LocalContract {
-//     async fn deposit_native_token(
-//         &self,
-//         _signer_private_key: H256,
-//         pubkey_salt_hash: Bytes32,
-//         amount: U256,
-//     ) -> Result<(), BlockchainError> {
-//         self.0.lock().unwrap().deposit(pubkey_salt_hash, 0, amount);
-//         Ok(())
-//     }
-// }
-
 use intmax2_zkp::ethereum_types::{bytes32::Bytes32, u256::U256};
 use serde::{Deserialize, Serialize};
 
