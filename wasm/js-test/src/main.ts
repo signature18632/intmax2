3157df71739af8373f23dedbd2103f012f340642
kbizikav <132550763+kbizikav@users.noreply.github.com>
2024-11-15 18:32:20 +0700

    wip

--- wasm/js-test/src/main.ts
+++ wasm/js-test/src/main.ts
@@ -1,4 +1,5 @@
-import { Config, generate_key_from_provisional, mimic_deposit, prepare_deposit,  } from '../pkg';
+import { Config, generate_key_from_provisional, get_user_data, mimic_deposit, prepare_deposit, sync, } from '../pkg';
+import { postEmptyBlock, syncValidityProof } from './state-manager';
 import { generateRandom32Bytes } from './utils';
 
 async function main() {
@@ -12,16 +13,30 @@ async function main() {
   const privateKey = key.privkey;
   console.log("privateKey: ", privateKey);
   console.log("publicKey: ", publicKey);
-  
+
 
   // deposit to the account
   const tokenIndex = 0; // 0 for ETH
-  const amount= "123";
-  const pubkeySaltHash = await prepare_deposit(config, privateKey, amount, 0);
+  const amount = "123";
+  const pubkeySaltHash = await prepare_deposit(config, privateKey, amount, tokenIndex);
+  console.log("pubkeySaltHash: ", pubkeySaltHash);
   await mimic_deposit(baseUrl, publicKey, amount);
 
+  // !The following two functions are not used in production.
+  await postEmptyBlock(baseUrl); // block builder post empty block
+  await syncValidityProof(baseUrl); // block validity prover sync validity proof
+  console.log("Deposit successfuly synced");
 
-  
+  // sync the account's balance proof 
+  await sync(config, privateKey);
+
+  // get the account's balance
+  const userData = await get_user_data(config, privateKey);
+  const balances = userData.balances;
+  for (let i = 0; i < balances.length; i++) {
+    const balance = balances[i];
+    console.log(`Token ${balance.token_index}: ${balance.amount}`);
+  }
 }
 
 main().then(() => {
