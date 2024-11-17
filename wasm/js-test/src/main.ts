1e2ff9e5588b15a9572ebee3437b6b6da8720766
kbizikav <132550763+kbizikav@users.noreply.github.com>
2024-11-17 11:30:36 +0700

    delete unused deps

--- wasm/js-test/src/main.ts
+++ wasm/js-test/src/main.ts
@@ -6,45 +6,42 @@ async function main() {
   const baseUrl = "http://localhost:9563";
   const config = Config.new(baseUrl, baseUrl, baseUrl, baseUrl, 3600n, 500n);
 
-  // // generate key
-  // const provisionalPrivateKey = generateRandom32Bytes();
-  // const key = await generate_key_from_provisional(provisionalPrivateKey);
-  // const publicKey = key.pubkey;
-  // const privateKey = key.privkey;
-  // console.log("privateKey: ", privateKey);
-  // console.log("publicKey: ", publicKey);
+  // generate key
+  const provisionalPrivateKey = generateRandom32Bytes();
+  const key = await generate_key_from_provisional(provisionalPrivateKey);
+  const publicKey = key.pubkey;
+  const privateKey = key.privkey;
+  console.log("privateKey: ", privateKey);
+  console.log("publicKey: ", publicKey);
 
-  // // deposit to the account
-  // const tokenIndex = 0; // 0 for ETH
-  // const amount = "123";
-  // const pubkeySaltHash = await prepare_deposit(config, privateKey, amount, tokenIndex);
-  // console.log("pubkeySaltHash: ", pubkeySaltHash);
-  // await mimic_deposit(baseUrl, pubkeySaltHash, amount);
+  // deposit to the account
+  const tokenIndex = 0; // 0 for ETH
+  const amount = "123";
+  const pubkeySaltHash = await prepare_deposit(config, privateKey, amount, tokenIndex);
+  console.log("pubkeySaltHash: ", pubkeySaltHash);
+  await mimic_deposit(baseUrl, pubkeySaltHash, amount);
 
-  // // !The following two functions are not used in production.
-  // await postEmptyBlock(baseUrl); // block builder post empty block
-  // await syncValidityProof(baseUrl); // block validity prover sync validity proof
-  // console.log("validity proof synced");
+  // !The following two functions are not used in production.
+  await postEmptyBlock(baseUrl); // block builder post empty block
+  await syncValidityProof(baseUrl); // block validity prover sync validity proof
+  console.log("validity proof synced");
 
-  // await new Promise((resolve) => setTimeout(resolve, 5000));
+  await new Promise((resolve) => setTimeout(resolve, 5000));
 
-  // // sync the account's balance proof 
-  // await sync(config, privateKey);
+  // sync the account's balance proof 
+  await sync(config, privateKey);
 
-  // console.log("Sync successful");
+  console.log("Sync successful");
 
-  // await new Promise((resolve) => setTimeout(resolve, 5000));
+  await new Promise((resolve) => setTimeout(resolve, 5000));
 
-  // // get the account's balance
-  // let userData = await get_user_data(config, privateKey);
-  // let balances = userData.balances;
-  // for (let i = 0; i < balances.length; i++) {
-  //   const balance = balances[i];
-  //   console.log(`Token ${balance.token_index}: ${balance.amount}`);
-  // }
-
-  const privateKey = "0x0ad9acdeb9930c6dcbe034284f45c348f45dc723ed67399d6931d135f3fab6b6"
-  const publicKey = "0x0029db243039870eb6da74dd69105cb57a977023bbe38ab232e059a677884f3a"
+  // get the account's balance
+  let userData = await get_user_data(config, privateKey);
+  let balances = userData.balances;
+  for (let i = 0; i < balances.length; i++) {
+    const balance = balances[i];
+    console.log(`Token ${balance.token_index}: ${balance.amount}`);
+  }
 
   // send a tx 
   const genericAddress = new JsGenericAddress(true, publicKey);
@@ -52,36 +49,36 @@ async function main() {
   const transfer = new JsTransfer(genericAddress, 0, "1", salt);
   const transfers = new Array<JsTransfer>();
   transfers.push(transfer);
-  const result = await send_tx_request(config, baseUrl, privateKey, transfers);
-  const tx = result.tx;
-  console.log("transfer tree root", tx.transfer_tree_root);
+  const memo = await send_tx_request(config, baseUrl, privateKey, transfers);
+  const tx = memo.tx();
+  console.log("tx.nonce", tx.nonce);
+  console.log("tx.transfer_tree_root", tx.transfer_tree_root);
 
-  // //! The following function is not used in production.
-  // await constructBlock(baseUrl); // block builder construct block
+  //! The following function is not used in production.
+  await constructBlock(baseUrl); // block builder construct block
 
-  // const proposal = await query_proposal(config, baseUrl, privateKey, tx);
-  // if (proposal === null) {
-  //   throw new Error("No proposal found");
-  // }
-  // const tx_memo = result.memo;
-  // await finalize_tx(config, baseUrl, privateKey, tx_memo, proposal);
+  const proposal = await query_proposal(config, baseUrl, privateKey, tx);
+  if (proposal === undefined) {
+    throw new Error("No proposal found");
+  }
+  await finalize_tx(config, baseUrl, privateKey, memo, proposal);
 
-  // // !The following function is not used in production.
-  // await postBlock(baseUrl); // block builder post block
-  // console.log("Tx successful");
+  // !The following function is not used in production.
+  await postBlock(baseUrl); // block builder post block
+  console.log("Tx successful");
 
-  // await new Promise((resolve) => setTimeout(resolve, 5000));
+  await new Promise((resolve) => setTimeout(resolve, 5000));
 
-  // // sync the account's balance proof
-  // await sync(config, privateKey);
-  // console.log("Sync successful");
+  // sync the account's balance proof
+  await sync(config, privateKey);
+  console.log("Sync successful");
 
-  // userData = await get_user_data(config, privateKey);
-  // balances = userData.balances;
-  // for (let i = 0; i < balances.length; i++) {
-  //   const balance = balances[i];
-  //   console.log(`Token ${balance.token_index}: ${balance.amount}`);
-  // }
+  userData = await get_user_data(config, privateKey);
+  balances = userData.balances;
+  for (let i = 0; i < balances.length; i++) {
+    const balance = balances[i];
+    console.log(`Token ${balance.token_index}: ${balance.amount}`);
+  }
 }
 
 main().then(() => {
