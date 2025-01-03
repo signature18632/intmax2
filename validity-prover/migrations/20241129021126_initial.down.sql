dd9ada4e877a129a8081c7a10274ac0c1654db56
kbizikav <132550763+kbizikav@users.noreply.github.com>
2025-01-03 12:08:21 +0700

    update query

--- validity-prover/migrations/20241129021126_initial.down.sql
+++ validity-prover/migrations/20241129021126_initial.down.sql
@@ -4,8 +4,8 @@ DROP TABLE IF EXISTS full_blocks;
 DROP TABLE IF EXISTS deposit_leaf_events;
 DROP TABLE IF EXISTS validity_state;
 DROP TABLE IF EXISTS tx_tree_roots;
-DROP TABLE IF EXISTS transition_proofs;
 DROP TABLE IF EXISTS validity_proofs;
+DROP TABLE IF EXISTS prover_tasks;
 
 -- Merkle tree tables
 DROP TABLE IF EXISTS hash_nodes;
--- validity-prover/src/app/prover_coordinator.rs
+++ validity-prover/src/app/prover_coordinator.rs
@@ -3,7 +3,10 @@ use std::time::Duration;
 use intmax2_zkp::circuits::validity::{
     transition::processor::TransitionProcessor, validity_circuit::ValidityCircuit,
 };
-use plonky2::{field::goldilocks_field::GoldilocksField, plonk::config::PoseidonGoldilocksConfig};
+use plonky2::{
+    field::goldilocks_field::GoldilocksField,
+    plonk::{config::PoseidonGoldilocksConfig, proof::ProofWithPublicInputs},
+};
 use sqlx::{postgres::PgPoolOptions, PgPool};
 
 use crate::Env;
@@ -15,6 +18,15 @@ type C = PoseidonGoldilocksConfig;
 const D: usize = 2;
 
 type Result<T> = std::result::Result<T, ProverCoordinatorError>;
+
+// CREATE TABLE IF NOT EXISTS prover_tasks (
+//     block_number INTEGER PRIMARY KEY,
+//     assigned BOOLEAN NOT NULL,
+//     assigned_at TIMESTAMP,
+//     completed BOOLEAN NOT NULL,
+//     completed_at TIMESTAMP,
+//     transition_proof JSONB
+// );
 
 pub struct ProverCoordinator {
     pub transition_processor: TransitionProcessor<F, C, D>,
@@ -43,8 +55,46 @@ impl ProverCoordinator {
         })
     }
 
-    // 既に生成されているtransition proofを使ってvalidity proofを生成する
-    pub fn wrap_task(&self) {
-        todo!()
+    // Assign the task with the smallest block number among the unassigned tasks
+    pub async fn assign_task(&self) -> Result<Option<u32>> {
+        let record = sqlx::query!(
+            r#"
+            UPDATE prover_tasks
+            SET assigned = TRUE, assigned_at = NOW()
+            WHERE block_number = (
+                SELECT block_number
+                FROM prover_tasks
+                WHERE assigned = FALSE
+                ORDER BY block_number
+                LIMIT 1
+            )
+            RETURNING block_number
+            "#,
+        )
+        .fetch_optional(&self.pool)
+        .await?;
+        let block_number = record.map(|r| r.block_number as u32);
+        Ok(block_number)
+    }
+
+    // Complete the task with the given block number
+    pub async fn complete_task(
+        &self,
+        block_number: u32,
+        transition_proof: &ProofWithPublicInputs<F, C, D>,
+    ) -> Result<()> {
+        let transition_proof = serde_json::to_value(transition_proof)?;
+        sqlx::query!(
+            r#"
+            UPDATE prover_tasks
+            SET assigned = FALSE, completed = TRUE, completed_at = NOW(), transition_proof = $1
+            WHERE block_number = $2
+            "#,
+            transition_proof,
+            block_number as i32,
+        )
+        .execute(&self.pool)
+        .await?;
+        Ok(())
     }
 }
