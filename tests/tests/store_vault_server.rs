c458b6b0c86e6365576d767f7ca9aa9e7fe1aaaf
kbizikav <132550763+kbizikav@users.noreply.github.com>
2025-01-04 08:17:34 +0700

    reset_user_data

--- tests/tests/store_vault_server.rs
+++ tests/tests/store_vault_server.rs
@@ -3,27 +3,26 @@ use intmax2_client_sdk::external_api::store_vault_server::StoreVaultServerClient
 use intmax2_interfaces::{
     api::store_vault_server::interface::StoreVaultClientInterface, data::user_data::UserData,
 };
-use intmax2_zkp::common::signature::key_set::KeySet;
+use intmax2_zkp::ethereum_types::u256::U256;
 use num_bigint::BigUint;
 use serde::Deserialize;
 
 #[derive(Deserialize)]
 struct EnvVar {
-    pub user_private_key: H256,
+    pub user_pubkey: H256,
     pub store_vault_server_base_url: String,
 }
 
 #[tokio::test]
-async fn test_store_vault_server() -> anyhow::Result<()> {
+async fn reset_user_data() -> anyhow::Result<()> {
     dotenv::dotenv().ok();
     let config = envy::from_env::<EnvVar>().unwrap();
 
-    let key = KeySet::new(BigUint::from_bytes_be(config.user_private_key.as_bytes()).into());
+    let user_pubkey: U256 = BigUint::from_bytes_be(config.user_pubkey.as_bytes()).try_into()?;
     let store_vault_server = StoreVaultServerClient::new(&config.store_vault_server_base_url);
-
-    let user_data = UserData::new(key.pubkey).encrypt(key.pubkey);
+    let user_data = UserData::new(user_pubkey).encrypt(user_pubkey);
     store_vault_server
-        .save_user_data(key.pubkey, user_data)
+        .save_user_data(user_pubkey, user_data)
         .await?;
     Ok(())
 }
