85a96ed1a35825e7b060fea04397bb736da6fd09
Baby Bear <132609968+smallbabybear@users.noreply.github.com>
2025-01-11 14:34:59 +0700

    Modify encrypt function

--- interfaces/src/data/encryption/algorithm.rs
+++ interfaces/src/data/encryption/algorithm.rs
@@ -18,33 +18,46 @@ use super::{
 
 pub use alloy_primitives::bytes::BytesMut;
 
-pub(crate) fn encrypt(_pubkey: U256, data: &[u8]) -> Vec<u8> {
-    // todo: add encryption
-    data.to_vec()
+const ENCRYPTION_VERSION: u8 = 1;
+
+pub(crate) fn encrypt(pubkey: U256, data: &[u8]) -> Vec<u8> {
+    let sender = EciesSender::new(pubkey);
+    let mut encrypted_data = BytesMut::new();
+    sender.encrypt_message(data, &mut encrypted_data);
+
+    let version = ENCRYPTION_VERSION;
+    let mut version_data = BytesMut::new();
+    version_data.extend_from_slice(&version.to_be_bytes());
+    version_data.unsplit(encrypted_data);
+
+    version_data.to_vec()
 }
 
-pub(crate) fn decrypt(_key: KeySet, encrypted_data: &[u8]) -> anyhow::Result<Vec<u8>> {
-    // todo: add decryption
-    Ok(encrypted_data.to_vec())
-}
-
-pub struct Ecies {
-    pub key: KeySet,
-    pub remote_public_key: Option<U256>,
-}
-
-impl Ecies {
-    pub fn new(key: KeySet, remote_public_key: U256) -> Self {
-        Self {
-            key,
-            remote_public_key: Some(remote_public_key),
-        }
+pub(crate) fn decrypt(key: KeySet, encrypted_data: &[u8]) -> anyhow::Result<Vec<u8>> {
+    let (version_data, encrypted_data) = encrypted_data.split_at(1);
+    let version = version_data[0];
+    if version != 1 {
+        anyhow::bail!("Unsupported version");
     }
 
-    pub fn new_server(key: KeySet) -> Self {
+    let mut encrypted_data = encrypted_data.to_vec();
+    let receiver = EciesReceiver::new(key);
+    let decrypted_data = receiver.decrypt_message(&mut encrypted_data)?;
+
+    Ok(decrypted_data.to_vec())
+}
+
+#[derive(Debug)]
+pub struct EciesSender {
+    pub receiver_public_key: U256,
+}
+
+impl EciesSender {
+    pub fn new(receiver_public_key: U256) -> Self {
+        // let nonce = rng.gen();
+        // let ephemeral_secret_key = SecretKey::new(&mut rng);
         Self {
-            key,
-            remote_public_key: None,
+            receiver_public_key,
         }
     }
 
@@ -59,8 +72,109 @@ impl Ecies {
         let key = KeySet::rand(&mut rng);
         out.extend_from_slice(&key.pubkey.to_bytes_be()); // 32 bytes
 
-        let remote_public_key = self.remote_public_key.unwrap();
-        let x = ecdh_x(&remote_public_key, &key.privkey);
+        let receiver_public_key = self.receiver_public_key;
+        let x = ecdh_x(&receiver_public_key, &key.privkey);
+        let mut key = [0u8; 32];
+        kdf(x, &[], &mut key);
+
+        let enc_key = B128::from_slice(&key[..16]);
+        let mac_key = sha256(&key[16..32]);
+
+        let iv: B128 = rng.gen();
+        let mut encryptor = Ctr64BE::<Aes128>::new((&enc_key.0).into(), (&iv.0).into());
+
+        let mut encrypted = data.to_vec();
+        encryptor.apply_keystream(&mut encrypted);
+
+        let tag = hmac_sha256(
+            mac_key.as_ref(),
+            &[iv.as_slice(), &encrypted],
+            &total_size.to_be_bytes(),
+        );
+
+        out.extend_from_slice(iv.as_slice());
+        out.extend_from_slice(&encrypted);
+        out.extend_from_slice(tag.as_ref());
+    }
+}
+
+#[derive(Debug)]
+pub struct EciesReceiver {
+    pub key: KeySet,
+}
+
+impl EciesReceiver {
+    pub fn new(key: KeySet) -> Self {
+        Self { key }
+    }
+
+    pub fn decrypt_message<'a>(&self, data: &'a mut [u8]) -> Result<&'a mut [u8], ECIESError> {
+        // parse the encrypted message from bytes
+        let encrypted_message = EncryptedMessage::parse(data)?;
+
+        // derive keys from the secret key and the encrypted message
+        let keys = encrypted_message.derive_keys(&self.key.privkey);
+
+        // check message integrity and decrypt the message
+        encrypted_message.check_and_decrypt(keys)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use aes::{
+        cipher::{KeyIvInit, StreamCipher},
+        Aes128,
+    };
+    use alloy_primitives::B128;
+    use ctr::Ctr64BE;
+    use intmax2_zkp::{
+        common::signature::key_set::KeySet,
+        ethereum_types::{u256::U256, u32limb_trait::U32LimbTrait},
+    };
+    use rand::Rng;
+
+    use super::{
+        decrypt, ecdh_x, encrypt, hmac_sha256, kdf, sha256, BytesMut, EciesReceiver, EciesSender,
+        U256_SIZE,
+    };
+
+    #[test]
+    fn test_ecies_encryption() {
+        let mut rand = rand::thread_rng();
+        let receiver_key = KeySet::rand(&mut rand);
+        let sender = EciesSender::new(receiver_key.pubkey);
+
+        let data = b"hello world";
+
+        let mut encrypted_data = BytesMut::new();
+        sender.encrypt_message(data, &mut encrypted_data);
+
+        let receiver = EciesReceiver::new(receiver_key);
+        let decrypted_data = receiver.decrypt_message(&mut encrypted_data).unwrap();
+
+        assert_eq!(data.to_vec(), decrypted_data);
+    }
+
+    pub fn encrypt_message_with_invalid_auth_tag(
+        receiver_public_key: U256,
+        data: &[u8],
+        out: &mut BytesMut,
+    ) {
+        let invalid_auth_tag = 0u16;
+
+        let mut rng = rand::thread_rng();
+
+        out.reserve(U256_SIZE + 16 + data.len() + 32);
+
+        let total_size: u16 = u16::try_from(U256_SIZE + 16 + data.len() + 32).unwrap();
+
+        out.extend_from_slice(&invalid_auth_tag.to_be_bytes());
+
+        let key = KeySet::rand(&mut rng);
+        out.extend_from_slice(&key.pubkey.to_bytes_be()); // 32 bytes
+
+        let x = ecdh_x(&receiver_public_key, &key.privkey);
         let mut key = [0u8; 32];
         kdf(x, &[], &mut key);
 
@@ -84,45 +198,74 @@ impl Ecies {
         out.extend_from_slice(tag.as_ref());
     }
 
-    pub fn decrypt_message<'a>(&self, data: &'a mut [u8]) -> Result<&'a mut [u8], ECIESError> {
-        // parse the encrypted message from bytes
-        let encrypted_message = EncryptedMessage::parse(data)?;
+    #[test]
+    fn test_ecies_encryption_with_invalid_auth_tag() {
+        let mut rand = rand::thread_rng();
+        let receiver_key = KeySet::rand(&mut rand);
 
-        // derive keys from the secret key and the encrypted message
-        let keys = encrypted_message.derive_keys(&self.key.privkey);
+        let data = b"hello world";
 
-        // check message integrity and decrypt the message
-        encrypted_message.check_and_decrypt(keys)
+        let mut encrypted_data = BytesMut::new();
+        encrypt_message_with_invalid_auth_tag(receiver_key.pubkey, data, &mut encrypted_data);
+
+        let receiver = EciesReceiver::new(receiver_key);
+        let decrypted_data = receiver.decrypt_message(&mut encrypted_data);
+
+        match decrypted_data {
+            Ok(_) => panic!("Decryption should fail"),
+            Err(e) => {
+                assert_eq!(e.to_string(), "tag check failure in read_header");
+            }
+        }
     }
-}
-
-#[cfg(test)]
-mod test {
-    use intmax2_zkp::common::signature::key_set::KeySet;
-
-    use super::{BytesMut, Ecies};
 
     #[test]
-    fn test_ecies_encryption() {
+    fn test_e2e_encryption() {
         let mut rand = rand::thread_rng();
-        let client_key = KeySet::rand(&mut rand);
-        let server_key = KeySet::rand(&mut rand);
-        let client = Ecies::new(client_key, server_key.pubkey);
+        let key = KeySet::rand(&mut rand);
 
         let data = b"hello world";
         println!("data: {:?}", data.to_vec());
 
-        let mut encrypted_data = BytesMut::new();
-        client.encrypt_message(data, &mut encrypted_data);
-        println!("encrypted data: {:#02x}", encrypted_data);
+        let encrypted_data = encrypt(key.pubkey, data);
+        println!("encrypted data: {:?}", encrypted_data);
 
-        // let version = 1u8;
-        // let version_data = version.to_be_bytes();
-
-        let server = Ecies::new_server(server_key);
-        let decrypted_data = server.decrypt_message(&mut encrypted_data).unwrap();
+        let decrypted_data = decrypt(key, &encrypted_data).unwrap();
         println!("decrypted data: {:?}", decrypted_data);
 
         assert_eq!(data.to_vec(), decrypted_data);
+    }
+
+    fn encrypt_with_unsupported_version(pubkey: U256, data: &[u8]) -> Vec<u8> {
+        // todo: add encryption
+        let sender = EciesSender::new(pubkey);
+        let mut encrypted_data = BytesMut::new();
+        sender.encrypt_message(data, &mut encrypted_data);
+
+        let version = 2u8;
+        let mut version_data = BytesMut::new();
+        version_data.extend_from_slice(&version.to_be_bytes());
+        version_data.unsplit(encrypted_data);
+
+        version_data.to_vec()
+    }
+
+    #[test]
+    fn test_e2e_encryption_with_unsupported_version() {
+        let mut rand = rand::thread_rng();
+        let key = KeySet::rand(&mut rand);
+
+        let data = b"hello world";
+
+        let encrypted_data = encrypt_with_unsupported_version(key.pubkey, data);
+
+        let decrypted_data = decrypt(key, &encrypted_data);
+
+        match decrypted_data {
+            Ok(_) => panic!("Decryption should fail"),
+            Err(e) => {
+                assert_eq!(e.to_string(), "Unsupported version");
+            }
+        }
     }
 }
