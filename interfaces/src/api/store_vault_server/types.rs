8064b96925c6428c722dd998cf904a0c156b111e
kbizikav <132550763+kbizikav@users.noreply.github.com>
2025-01-12 21:21:34 +0700

    add auth

--- interfaces/src/api/store_vault_server/types.rs
+++ interfaces/src/api/store_vault_server/types.rs
@@ -1,6 +1,5 @@
-use super::interface::SaveDataEntry;
-use crate::data::meta_data::MetaData;
-use intmax2_zkp::{common::signature::flatten::FlatG2, ethereum_types::u256::U256};
+use super::interface::{DataType, SaveDataEntry};
+use crate::{data::meta_data::MetaData, utils::signature::Auth};
 use serde::{Deserialize, Serialize};
 use serde_with::{base64::Base64, serde_as};
 
@@ -8,9 +7,7 @@ use serde_with::{base64::Base64, serde_as};
 #[serde(rename_all = "camelCase")]
 pub struct BatchSaveDataRequest {
     pub data: Vec<SaveDataEntry>,
-    pub pubkey: U256,
-    pub expiry: u64,
-    pub signature: FlatG2,
+    pub auth: Auth,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -21,27 +18,23 @@ pub struct BatchSaveDataResponse {
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct GetUserDataRequestWithSignature {
-    pub expiry: u64,
-    pub signature: FlatG2,
+pub struct GetUserDataRequest {
+    pub auth: Auth,
 }
 
 #[serde_as]
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct GetUserDataResponse {
-    pub is_exist: bool,
-    #[serde_as(as = "Base64")]
-    pub data: Vec<u8>,
+    #[serde_as(as = "Option<Base64>")]
+    pub data: Option<Vec<u8>>,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct GetDataAllAfterQuery {
-    pub pubkey: U256,
-    pub timestamp: u64,
-    pub expiry: u64,
-    pub signature: FlatG2,
+    pub data_type: DataType,
+    pub auth: Auth,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
--- interfaces/src/utils/signature.rs
+++ interfaces/src/utils/signature.rs
@@ -1,7 +1,7 @@
 use ark_bn254::{Bn254, Fq, Fr, G1Affine, G2Affine};
 use ark_ec::{pairing::Pairing, AffineRepr};
 use intmax2_zkp::{
-    common::signature::flatten::FlatG2,
+    common::signature::{flatten::FlatG2, key_set::KeySet},
     ethereum_types::{bytes32::Bytes32, u256::U256, u32limb_trait::U32LimbTrait},
 };
 use num_traits::identities::Zero;
@@ -9,9 +9,84 @@ use plonky2::field::{goldilocks_field::GoldilocksField, types::Field};
 use plonky2_bn254::{
     curves::g2::G2Target, fields::recover::RecoverFromX, utils::hash_to_g2::HashToG2,
 };
-use plonky2_keccak::utils::solidity_keccak256;
+use serde::{Deserialize, Serialize};
+use sha2::Digest;
 
-pub fn hash_to_message_point(hash: Bytes32) -> G2Affine {
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Auth {
+    pub pubkey: U256,
+    pub expiry: u64,
+    pub signature: FlatG2,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SignContent {
+    pub pubkey: U256,
+    pub content: Vec<u8>,
+    pub expiry: u64,
+}
+
+impl Auth {
+    pub fn sign(key: KeySet, expiry: u64, content: &[u8]) -> Self {
+        let sign_content = SignContent {
+            pubkey: key.pubkey,
+            content: content.to_vec(),
+            expiry,
+        };
+        let serialized = bincode::serialize(&sign_content).unwrap();
+        let digest = sha2::Sha256::digest(&serialized);
+        let hash = Bytes32::from_bytes_be(&digest);
+        let signature = sign_message(key.privkey, hash).unwrap();
+        Auth {
+            pubkey: key.pubkey,
+            expiry,
+            signature,
+        }
+    }
+
+    pub fn verify(&self, content: &[u8]) -> anyhow::Result<()> {
+        if self.expiry
+            < std::time::SystemTime::now()
+                .duration_since(std::time::UNIX_EPOCH)?
+                .as_secs()
+        {
+            anyhow::bail!("Signature expired");
+        }
+        let sign_content = SignContent {
+            pubkey: self.pubkey,
+            content: content.to_vec(),
+            expiry: self.expiry,
+        };
+        let serialized = bincode::serialize(&sign_content).unwrap();
+        let digest = sha2::Sha256::digest(&serialized);
+        let hash = Bytes32::from_bytes_be(&digest);
+        verify_signature(self.signature.clone(), self.pubkey, hash)
+    }
+}
+
+pub fn sign_message(priv_key: Fr, hash: Bytes32) -> anyhow::Result<FlatG2> {
+    let message_point = hash_to_message_point(hash);
+    let signature: G2Affine = (message_point * priv_key).into();
+    Ok(FlatG2::from(signature))
+}
+
+pub fn verify_signature(signature: FlatG2, pubkey: U256, hash: Bytes32) -> anyhow::Result<()> {
+    let pubkey_x: Fq = pubkey.into();
+    let pubkey_g1 = G1Affine::recover_from_x(pubkey_x);
+    let g1_generator_inv = -G1Affine::generator();
+    let message_g2 = hash_to_message_point(hash);
+    let signature_g2 = G2Affine::from(signature);
+    let g1s = vec![g1_generator_inv, pubkey_g1];
+    let g2s = vec![signature_g2, message_g2];
+    if !check_pairing(g1s, g2s) {
+        anyhow::bail!("Invalid signature");
+    }
+    Ok(())
+}
+
+fn hash_to_message_point(hash: Bytes32) -> G2Affine {
     let elements = hash
         .to_u32_vec()
         .iter()
@@ -20,69 +95,37 @@ pub fn hash_to_message_point(hash: Bytes32) -> G2Affine {
     G2Target::<GoldilocksField, 2>::hash_to_g2(&elements)
 }
 
-/// Convert the message into a format that can be signed, using the same method as when signing the tx tree root.
-fn message_to_point(mut message: Vec<u8>) -> G2Affine {
-    let mut message_u32_slice = vec![];
-    while message.len() % 4 != 0 {
-        message.push(0);
-    }
-
-    for i in 0..message.len() / 4 {
-        let mut u32_bytes = [0u8; 4];
-        u32_bytes.copy_from_slice(&message[i * 4..(i + 1) * 4]);
-        message_u32_slice.push(u32::from_be_bytes(u32_bytes));
-    }
-    let message_hash = solidity_keccak256(&message_u32_slice);
-    let message = Bytes32::from_u32_slice(&message_hash);
-
-    hash_to_message_point(message)
-}
-
-pub fn sign_message(priv_key: Fr, message: Vec<u8>) -> anyhow::Result<FlatG2> {
-    let message_point = message_to_point(message);
-
-    let signature: G2Affine = (message_point * priv_key).into();
-
-    Ok(FlatG2::from(signature))
-}
-
 fn check_pairing(g1s: Vec<G1Affine>, g2s: Vec<G2Affine>) -> bool {
     Bn254::multi_pairing(g1s, g2s).is_zero()
-}
-
-pub fn verify_signature(signature: FlatG2, pubkey: U256, message: Vec<u8>) -> anyhow::Result<()> {
-    let pubkey_x: Fq = pubkey.into();
-    let pubkey_g1 = G1Affine::recover_from_x(pubkey_x);
-    let g1_generator_inv = -G1Affine::generator();
-    let message_g2 = message_to_point(message);
-    let signature_g2 = G2Affine::from(signature);
-    let g1s = vec![g1_generator_inv, pubkey_g1];
-    let g2s = vec![signature_g2, message_g2];
-
-    if !check_pairing(g1s, g2s) {
-        anyhow::bail!("Invalid signature");
-    }
-
-    Ok(())
 }
 
 #[cfg(test)]
 mod test {
     use super::{sign_message, verify_signature};
-    use intmax2_zkp::common::signature::key_set::KeySet;
+    use intmax2_zkp::{
+        common::signature::key_set::KeySet,
+        ethereum_types::{bytes32::Bytes32, u32limb_trait::U32LimbTrait as _},
+    };
 
     #[test]
     fn test_sign_verify() {
         let mut rnd = rand::thread_rng();
         let key = KeySet::rand(&mut rnd);
-        let message = vec![1, 2, 3, 4, 5, 6, 7];
-        let signature = sign_message(key.privkey, message.clone()).unwrap();
-        assert!(verify_signature(signature.clone(), key.pubkey, message).is_ok());
+        let hash = Bytes32::rand(&mut rnd);
+        let signature = sign_message(key.privkey, hash).unwrap();
+        assert!(verify_signature(signature.clone(), key.pubkey, hash).is_ok());
+    }
 
-        let different_message = vec![1, 2, 3, 4, 5, 6, 8];
-        assert!(verify_signature(signature.clone(), key.pubkey, different_message).is_err());
-
-        let different_message = vec![1, 2, 3, 4];
-        assert!(verify_signature(signature, key.pubkey, different_message).is_err());
+    #[test]
+    fn test_auth_verify() {
+        let mut rnd = rand::thread_rng();
+        let key = KeySet::rand(&mut rnd);
+        let content = b"test";
+        let current_time = std::time::SystemTime::now()
+            .duration_since(std::time::UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
+        let auth = super::Auth::sign(key, current_time + 10, content);
+        assert!(auth.verify(content).is_ok());
     }
 }
