a76d55b6dbbb185f188993f18300fe8f1ea762fe
kbizikav <132550763+kbizikav@users.noreply.github.com>
2025-01-14 16:50:28 +0700

    delete sync from sync withdrawal

--- cli/src/cli/sync.rs
+++ cli/src/cli/sync.rs
@@ -4,7 +4,6 @@ use super::{client::get_client, error::CliError};
 
 pub async fn sync_withdrawals(key: KeySet) -> Result<(), CliError> {
     let client = get_client()?;
-    client.sync(key).await?;
     client.sync_withdrawals(key).await?;
     Ok(())
 }
